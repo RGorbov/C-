977f32a87b57a4639e3a76700c6b70d7859feb93
Aleksander <alexgidro91@mail.ru>
2023-03-09 21:57:12 +0700

    Homework 1 was added

---
+++ HW_01/task_2/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+char[] delimiterChars = { ' ', ',' };
+
+Console.Write("Enter three space separated digits: ");
+int[] numbers = (
+    Console
+        .ReadLine()
+        .Split(delimiterChars)
+        .Where(x => !string.IsNullOrEmpty(x))
+        .Select(int.Parse)
+        .ToArray());
+
+if (numbers.Length != 3)
+{
+    throw new InvalidOperationException("Exactly three digits must me entered.");
+}
+
+// Вывод списка чисел
+foreach (var number in numbers)
+{
+    Console.Write($"{number} ");
+}
+Console.WriteLine();
+
+int max = numbers[0];
+for (int i = 1; i < 3; i++)
+{
+    if (numbers[i] > max) max = numbers[i];
+}
+
+Console.WriteLine($"Maximum value is: {max}");
