8e0fcb92150d6a048f23e464e410a49e094647f9
Roman Gorbov <romangorbov86@gmail.com>
2023-03-10 21:56:25 +0700

    Homework 4 was added

---
+++ HW_04/task_3/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] GenerateArray(int ArrayLength) {
+    int[] MyArray = new int[ArrayLength];
+    Console.WriteLine("Start writing to MyArray (Info just to check out): ");
+    for (int i=0; i<MyArray.Length; i++) {
+        MyArray[i] = new Random().Next(1, 100);
+        Console.Write($"{MyArray[i]} ");
+    }
+    Console.WriteLine();
+    return MyArray;
+}
+
+void WriteArray(int[] MyArray) {
+    Console.WriteLine("Printing MyArray:");
+    foreach (var elem in MyArray) {
+        Console.Write($"{elem} ");
+    }
+    Console.WriteLine();
+}
+
+
+int[] MyArray1 = GenerateArray(8);
+WriteArray(MyArray1);
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+WriteArray(new int[] {1, 2, 5, 7, 19});
+
+// 6, 1, 33 -> [6, 1, 33]
+WriteArray(new int[] {6, 1, 33});
