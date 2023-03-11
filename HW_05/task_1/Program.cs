61ea93608d533226df7bf629eac087621e841898
Aleksander <alexgidro91@mail.ru>
2023-03-11 20:56:10 +0700

    Homework 5 was added

---
+++ HW_05/task_1/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+void PrintArray(int[] MyArray)
+{
+    foreach (var elem in MyArray)
+    {
+        Console.Write($"{elem} ");
+    }
+    Console.WriteLine();
+}
+
+int[] GenerateRandArray()
+{
+    int[] MyArray = new int[new Random().Next(3, 8)];
+    for (int i = 0; i < MyArray.Length; i++)
+    {
+        MyArray[i] = new Random().Next(100, 999);
+    }
+    Console.Write("Generated array for you: ");
+    PrintArray(MyArray);
+    return MyArray;
+}
+
+int CountEvenNumbers(int[] MyArray)
+{
+    int EvenCount = 0;
+    for (int i = 0; i < MyArray.Length; i++)
+    {
+        if (MyArray[i] % 2 == 0) EvenCount++;
+    }
+    return EvenCount;
+}
+
+int[] MyArray1 = GenerateRandArray();
+Console.WriteLine($"There is {CountEvenNumbers(MyArray1)} even numbers in MyArray1.");
---
+++ HW_05/task_2/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+void PrintArray(int[] MyArray) {
+    foreach (var elem in MyArray)
+    {
+        Console.Write($"{elem} ");
+    }
+    Console.WriteLine();
+}
+
+int[] GenerateRandArray() {
+    int[] MyArray = new int[new Random().Next(1,8)];
+    for (int i=0; i < MyArray.Length; i++) {
+        MyArray[i] = new Random().Next(-100, 100);
+    }
+    Console.Write("Generated array for you: ");
+    PrintArray(MyArray);
+    return MyArray;
+}
+
+int CountOddPosNumbers(int[] MyArray) {
+    int NumSum = 0;
+    for (int i=0; i<MyArray.Length; i++) { 
+        if (i % 2 != 0) NumSum += MyArray[i];
+    }
+    return NumSum;
+}
+
+int[] MyArray1 = GenerateRandArray();
+Console.WriteLine($"Sum of numbers at odd position is: {CountOddPosNumbers(MyArray1)}");
