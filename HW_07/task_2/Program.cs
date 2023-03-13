86d6c99bbb7bb2abb5914f2084f501e61861fe55
Aleksander <alexgidro91@mail.ru>
2023-03-13 18:12:56 +0700

    Homework 7 was added.

---
+++ HW_07/task_2/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+void GetElementByPosition(int[,] matrix, int position)
+{
+    if (position > matrix.GetLength(0) * matrix.GetLength(1))
+    {
+        Console.WriteLine("There is no element at this position. Out of index.");
+    }
+    else
+    {
+        int row = position / matrix.GetLength(1);
+        int column = position % matrix.GetLength(1);
+        Console.WriteLine($"Position: {position}. Value: {matrix[row, column]}.");
+    }
+}
+
+
+int[,] MyArray = 
+{
+    {1, 4, 7, 2},
+    {5, 9, 2, 3},
+    {8, 4, 2, 4}
+};
+
+GetElementByPosition(MyArray, 3);
+Console.WriteLine();
+GetElementByPosition(MyArray, 17);
---
+++ HW_07/task_3/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] GenerateMatrix(int rows, int columns, int MinValue = -100, int MaxValue = 101)
+{
+    int[,] matrix = new int[rows, columns];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(MinValue, MaxValue);
+        }
+    }
+    return matrix;
+}
+
+double[] GetColumnAvg(int[,] matrix) 
+{
+    int ColumnsNumber = matrix.GetLength(1);
+    int RowsNumber = matrix.GetLength(0);
+    double[] ColumnAvg = new double[ColumnsNumber];
+
+    for (int column=0; column < ColumnsNumber; column++)
+    {
+        for (int row=0; row < RowsNumber; row++)
+        {
+            ColumnAvg[column] += Convert.ToDouble(matrix[row, column]) / RowsNumber;
+        }
+        ColumnAvg[column] = Math.Round(ColumnAvg[column], 2);
+    }
+    return ColumnAvg;
+}
+
+int[,] MyMatrix = GenerateMatrix(3, 4, 0, 11);
+Console.WriteLine("Matrix below was generated: ");
+PrintMatrix(MyMatrix);
+
+Console.WriteLine();
+
+Console.WriteLine("Average of columns values: ");
+foreach (var element in GetColumnAvg(MyMatrix)) { Console.Write($"{element} "); }
+Console.WriteLine();
